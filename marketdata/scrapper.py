bbbe1294495b3fc7b9c471ee4ec19f43a88b689d
Nilmani Jha <nilmanijha06@gmail.com>
2026-08-05 20:29:04 +0530

    Add stock data fetching function using yfinance

--- marketdata/scrapper.py
+++ marketdata/scrapper.py
@@ -1,1 +1,8 @@
+import yfinance as yf
 
+def fetch_stock_ohlcv(ticker='AAPL', period='1mo', interval='1d'):
+    data = yf.download(tickers=ticker, period=period, interval=interval)
+    # Standardizes columns: Open, High, Low, Close, Volume
+    return data[['Open', 'High', 'Low', 'Close', 'Volume']]
+
+print(fetch_stock_ohlcv('RELIANCE.NS'))
